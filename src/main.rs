308410135b574a99ef8a8fc2daac4ff54e2d6094
Terje Engelbertsen <terje.engelbertsen@mogrouppartners.com>
2023-12-05 13:35:47 +0100

    Prepared for docker and such.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().route("/", web::get().to(index))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
